f9beea3e3aac7402051600e04971162a23c2307a
mamamamamaa <maks.denishchuk@gmail.com>
2023-01-14 19:43:40 +0200

    start playlist card

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,20 @@ import { Layout } from "../components/Layout/Layout";
 import { Logo } from "../components/Logo/Logo";
 import { Navbar } from "../components/Navbar/Navbar";
 import { Searchbar } from "../components/Searchbar/Searchbar";
+import { PlaylistCard } from "../components/PlaylistCard/PlaylistCard";
 
 const App: FC = () => {
   return (
     <>
       <Layout>
         <div className="flex flex-col items-center gap-6">
-          <Logo />
-          <Navbar />
+          {/*<Logo />*/}
+          {/*<Navbar />*/}
         </div>
-        <Searchbar />
+        <div>
+          {/*<Searchbar />*/}
+          <PlaylistCard />
+        </div>
       </Layout>
     </>
   );
