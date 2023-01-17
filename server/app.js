225ddfda42c3842a1e9923aba971fb5c5c90dc03
mamamamamaa <maks.denishchuk@gmail.com>
2023-01-17 20:50:42 +0200

    //

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
+require("dotenv").config();
 
 const app = express();
+const { PORT, CLIENT_ID, CLIENT_SECRET, REDIRECT_LINK } = process.env;
 
 app.post("/login", (req, res) => {
   const { code } = req.body;
   const spotifyApi = new SpotifyWebApi({});
 });
+
+app.listen(PORT, () => console.log(`Server is running on the ${PORT} port`));
