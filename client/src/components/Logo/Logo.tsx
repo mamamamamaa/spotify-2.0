d8aae5be6f491d3d2d43866dd99e956ceb74135b
mamamamamaa <maks.denishchuk@gmail.com>
2023-01-25 19:48:08 +0200

    add logo

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,8 +1,10 @@
 import { FC } from "react";
 
 // @ts-ignore
+import style from "./Logo.module.css";
+// @ts-ignore
 import LogoIcon from "../../icons/Logo.svg";
 
 export const Logo: FC = () => {
-  return <img src={LogoIcon} alt="Spotify logo" className="mx-auto my-5" />;
+  return <img src={LogoIcon} alt="Spotify logo" className={style.log} />;
 };
