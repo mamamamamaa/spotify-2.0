f12ba9c98de750099c007eb2d844c8ccc56cd277
mamamamamaa <maks.denishchuk@gmail.com>
2023-01-25 21:54:01 +0200

    add loco style's module file

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react";
-
-// @ts-ignore
-import style from "./Logo.module.css";
 // @ts-ignore
 import LogoIcon from "../../icons/Logo.svg";
 
+// @ts-ignore
+import style from "./Logo.module.css";
+
 export const Logo: FC = () => {
-  return <img src={LogoIcon} alt="Spotify logo" className={style.log} />;
+  return <img src={LogoIcon} alt="Spotify logo" className={style.logo} />;
 };
