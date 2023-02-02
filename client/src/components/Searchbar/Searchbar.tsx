0c3727111c807722a2e578f034365dde955c5de0
mamamamamaa <maks.denishchuk@gmail.com>
2023-02-02 11:38:54 +0200

    ///

--- client/src/components/Searchbar/Searchbar.tsx
+++ client/src/components/Searchbar/Searchbar.tsx
@@ -3,6 +3,7 @@ import { BaseSyntheticEvent, FC } from "react";
 import { FiSearch } from "react-icons/fi";
 import { useAppDispatch, useSongs } from "../../redux/hooks";
 import { searchTracks } from "../../redux/tracks";
+import style from "./Searchbar.module.css";
 
 export const Searchbar: FC = () => {
   const dispatch = useAppDispatch();
@@ -11,8 +12,8 @@ export const Searchbar: FC = () => {
     !isLoading && dispatch(searchTracks(e.target.value));
 
   return (
-    <form className="flex justify-center w-full">
-      <label className="relative w-full">
+    <form className={style.formWrapper}>
+      <label className={style.labelWrapper}>
         <FiSearch
           className="absolute top-2.5 left-2 text-gray hover:text-white ease-in-out duration-300 cursor-pointer"
           size={30}
