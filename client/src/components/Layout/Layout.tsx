3b0e4d12fea64c46e627e2f1f21b068d3a600e66
mamamamamaa <maks.denishchuk@gmail.com>
2023-03-16 11:13:41 +0200

    fix up imports

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,8 +1,10 @@
 import { FC, Suspense } from "react";
 import { Outlet } from "react-router-dom";
+
 import { Logo } from "../Logo/Logo";
 import { Playback } from "../Playback/Playback";
 import { Navigation } from "../Navigation/Navigation";
+
 import style from "./Layout.module.css";
 
 export const Layout: FC = () => {
